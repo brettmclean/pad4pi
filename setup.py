fd8c231effefa146cc83cb37686e2227768cd8cb
Brett McLean <brettrmclean@gmail.com>
2015-07-25 22:58:35 -0600

    Include README as package long_description

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name="pad4pi",
 	version="1.0.0",
 	description="Interrupt-based matrix keypad library for Raspberry Pi",
+	long_description=readme(),
 	url="https://github.com/brettmclean/pad4pi",
 	author="Brett McLean",
 	author_email="brettrmclean@gmail.com",
