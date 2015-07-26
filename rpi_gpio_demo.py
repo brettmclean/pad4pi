9ae7286dd93041d04bad590ba08d177fae1b6439
Brett McLean <brettrmclean@gmail.com>
2015-07-26 16:03:53 -0600

    Move setup code into try block (ensures GPIO cleanup if something goes wrong)

--- rpi_gpio_demo.py
+++ rpi_gpio_demo.py
@@ -3,15 +3,15 @@
 from pad4pi import rpi_gpio
 import time
 
-factory = rpi_gpio.KeypadFactory()
-keypad = factory.create_4_by_3_keypad() # makes assumptions about keypad layout and GPIO pin numbers
-
 def print_key(key):
 	print(key)
 
-keypad.registerKeyPressHandler(print_key)
-
 try:
+	factory = rpi_gpio.KeypadFactory()
+	keypad = factory.create_4_by_3_keypad() # makes assumptions about keypad layout and GPIO pin numbers
+
+	keypad.registerKeyPressHandler(print_key)
+
 	print("Press buttons on your keypad. Ctrl+C to exit.")
 	while True:
 		time.sleep(1)
