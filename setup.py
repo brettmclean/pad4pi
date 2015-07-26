32ec601bb8828202396697281b47e7e4a10c3085
Brett McLean <brettrmclean@gmail.com>
2015-07-25 18:44:51 -0600

    Commit package and demo files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="pad4pi",
+	version="1.0",
+	description="Interrupt-based matrix keypad library for Raspberry Pi",
+	url="https://github.com/brettmclean/pad4pi",
+	author="Brett McLean",
+	author_email="brettrmclean@gmail.com",
+	license="GPLv3",
+	packages=["pad4pi"]
+	)
