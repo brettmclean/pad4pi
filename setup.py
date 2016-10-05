50da59357ae093427a2892c4362d71d4491dcbd6
Thijs Triemstra <info@collab.nl>
2016-10-05 20:16:32 +0200

    add classifiers

--- setup.py
+++ setup.py
@@ -5,16 +5,26 @@ def readme():
         return f.read()
 
 setup(name="pad4pi",
-	version="1.1.1",
-	description="Interrupt-based matrix keypad library for Raspberry Pi",
-	long_description=readme(),
-	url="https://github.com/brettmclean/pad4pi",
-	keywords="raspberry pi matrix keypad",
-	author="Brett McLean",
-	author_email="brettrmclean@gmail.com",
-	license="LGPLv3",
-	packages=["pad4pi"],
-	install_requires=[
-		"RPi.GPIO"
-	]
-	)
+    version="1.1.1",
+    description="Interrupt-based matrix keypad library for Raspberry Pi",
+    long_description=readme(),
+    url="https://github.com/brettmclean/pad4pi",
+    keywords="raspberry pi matrix keypad",
+    author="Brett McLean",
+    author_email="brettrmclean@gmail.com",
+    license="LGPLv3",
+    packages=["pad4pi"],
+    install_requires=[
+        "RPi.GPIO"
+    ],
+    classifiers = [
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Education",
+        "Intended Audience :: Developers",
+        "Topic :: Education",
+        "Topic :: System :: Hardware",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3"
+    ]
+)
